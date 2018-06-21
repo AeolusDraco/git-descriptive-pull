ef575affc36d4debcaca6d7d72f7674b0b2e6fbd
AeolusDraco <adamjh51@gmail.com>
2018-06-21 14:16:31 -0500

    Remove trailing .git from directory notification

--- update_git_repos.sh
+++ update_git_repos.sh
@@ -9,7 +9,7 @@ echo -e "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -name ".git" | cut -c 3-); do
     echo -e "";
-    echo -e "\033[33m"+$i+"\033[0m";
+    echo -e "\033[33m"+$(echo $i | sed 's/.\{5\}$//')+"\033[0m";
 
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
